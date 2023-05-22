6cee1b09bc4f333d1ca48f1efd8a4c2c4b77e69b
Mohanraj Selvam <c-mohanraj.selvam@charter.com>
2023-05-22 16:38:10 +0530

    Added undo actions

--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -8,7 +8,23 @@ describe('When: Use the search feature', () => {
 
     cy.get('form').submit();
 
-    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
+    cy.get('.book ').should('have.length.greaterThan', 1);
+  });
+
+  it('Then: I should able to undo my reading list', () => {
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get('.book ').should('have.length.greaterThan', 1);
+    cy.get('[data-testing="book-item-0"]')
+      .find('[data-cy-add-book="add-book-0"]')
+      .click();
+    cy.get('.mat-simple-snackbar-action')
+      .find('.mat-button-wrapper')
+      .contains('Undo')
+      .click();
+    cy.get('[data-testing="book-item-0"]')
+      .find('[data-cy-add-book="add-book-0"]')
+      .should('be.enabled');
   });
 
   xit('Then: I should see search results as I am typing', () => {
--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,17 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedTestingModule } from '@tmo/shared/testing';
+import { createBook, SharedTestingModule } from '@tmo/shared/testing';
 
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBarHarness } from '@angular/material/snack-bar/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 
 describe('ProductsListComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
+      imports: [BooksFeatureModule, NoopAnimationsModule, MatSnackBarModule, SharedTestingModule]
     }).compileComponents();
   }));
 
@@ -19,9 +24,29 @@ describe('ProductsListComponent', () => {
     fixture = TestBed.createComponent(BookSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeDefined();
   });
+
+  it('should be able to check whether snack-bar has action', async () => {
+    const mockRemoveData = createBook('C');
+    fixture.componentInstance.snackBarOpen('success', mockRemoveData, 'Undo');
+    const snackBar = await loader.getHarness(MatSnackBarHarness);
+    expect(await snackBar.hasAction()).toBe(true);
+  }, 10000);
+
+  it('should be able to undo from the list', async () => {
+    spyOn(component, 'removeFromReadingList');
+    const mockRemoveData = createBook('C');
+    fixture.componentInstance.snackBarOpen('With action', mockRemoveData, 'Undo');
+    const snackBar = await loader.getHarness(MatSnackBarHarness);
+    expect(await snackBar.getActionDescription()).toBe('Undo');
+
+    snackBar.dismissWithAction();
+    component.removeFromReadingList(mockRemoveData);
+    expect(component.removeFromReadingList).toHaveBeenCalled();
+  }, 10000);
 });
